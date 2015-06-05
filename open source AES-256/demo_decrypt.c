10b7bba37fb0a7424b8b65a9f7317e9df3db7098
Park, Hyung Soon <starvesperia@naver.com>
2015-06-05 22:56:17 +0900

    Used open source AES-256.
    Open source from : http://www.literatecode.com
    Implemented only demo.c, demo_decrypt.c
    
    This is ECB mode AES-256 Encryption/Decryption.
    It was too slow to use.

---
+++ open source AES-256/demo_decrypt.c
@@ -0,0 +1,65 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "aes.h"
+
+#define DECODED_FILE_NAME "decrypted.txt"
+#define ENCODED_FILE_NAME "secured.txt.enc"
+
+int main (int argc, char *argv[])
+{
+    aes256_context ctx;
+	FILE *in_file;
+	long fileSize;
+	FILE *out_file;
+    uint8_t key[32];
+	uint8_t i;
+	uint8_t fileBuf[16];
+	uint8_t *tmpBuf;
+	time_t start, end;
+	float gap;
+
+	in_file = fopen(ENCODED_FILE_NAME, "rb");
+	out_file = fopen(DECODED_FILE_NAME, "wb");
+	if(in_file == NULL) exit(1);
+
+	fseek(in_file, 0, SEEK_END);
+	fileSize = ftell(in_file);
+	rewind(in_file);
+	
+	//get a test key
+	for (i = 0; i < sizeof(key);i++) key[i] = i;
+	
+	start = clock();
+
+	while( fread(fileBuf, 1, sizeof(fileBuf), in_file) )
+	{
+		aes256_init(&ctx, key);
+		aes256_decrypt_ecb(&ctx, fileBuf);
+		
+		fwrite(fileBuf, 1, sizeof(fileBuf), out_file);
+		fileSize -= sizeof(fileBuf);
+
+		if(0 < fileSize && fileSize < sizeof(fileBuf))
+		{
+			tmpBuf = (uint8_t *)malloc(sizeof(uint8_t)*fileSize);
+			fread(tmpBuf, 1, fileSize, in_file);
+			
+			aes256_init(&ctx, key);
+			aes256_decrypt_ecb(&ctx, tmpBuf);
+			
+			fwrite(tmpBuf, 1, fileSize, out_file);
+			break;
+		}
+	}
+	
+	aes256_done(&ctx);
+
+	end = clock();
+	gap = (float) (end - start)/CLOCKS_PER_SEC;
+	printf("걸린시간 : %f초", gap);
+
+	fclose(in_file);
+	fclose(out_file);
+    return 0;
+}
